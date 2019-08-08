d58f9c6d90967f3839bc0e3af7ce2e202859b617
Emily G <aspentree79@gmail.com>
2019-08-08 12:19:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -88,28 +88,27 @@ programmer_hash
 end
 
 
-#
-# def changing_alan
-# 	programmer_hash =
-#  		{
-#         :grace_hopper => {
-#           :known_for => "COBOL",
-#           :languages => ["COBOL", "FORTRAN"]
-#         },
-#         :alan_kay => {
-#           :known_for => "Object Orientation",
-#           :languages => ["Smalltalk", "LISP"]
-#         },
-#         :dennis_ritchie => {
-#           :known_for => "Unix",
-#           :languages => ["C"]
-#         }
-#      }
-#      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-#      alans_new_info = "GUI"
-#
-#
-# end
+def changing_alan
+	programmer_hash =
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash
+
+
+end
 #
 # def adding_to_dennis
 # 	programmer_hash =
