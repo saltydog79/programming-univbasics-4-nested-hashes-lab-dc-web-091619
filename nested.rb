fdd95acb38f9e9bebd88a4f161b9c513d4c2f254
Emily G <aspentree79@gmail.com>
2019-08-08 12:25:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -108,22 +108,23 @@ def changing_alan
   programmer_hash
 end
 
-#
-# def adding_to_dennis
-# 	programmer_hash =
-#  		{
-#         :grace_hopper => {
-#           :known_for => "COBOL",
-#           :languages => ["COBOL", "FORTRAN"]
-#         },
-#         :alan_kay => {
-#           :known_for => "Object Orientation",
-#           :languages => ["Smalltalk", "LISP"]
-#         },
-#         :dennis_ritchie => {
-#           :known_for => "Unix",
-#           :languages => ["C"]
-#         }
-#      }
-#
-# end
+
+def adding_to_dennis
+	programmer_hash =
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+
+programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+end
